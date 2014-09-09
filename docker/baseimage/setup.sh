e77e70d7fffe7ce9eafe0049ae23c2f3ddcb313c
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2014-09-08 19:53:07 -0500

    simplify build,install for ag,tmux,watchman

--- docker/baseimage/setup.sh
+++ docker/baseimage/setup.sh
@@ -23,16 +23,10 @@ adduser --disabled-password --gecos "" sailor
 sudo -i -u sailor /docker-build/support/user_sailor.sh
 sudo -i -u sailor /docker-build/support/user_common.sh
 
-# Build, install: tmux
-/docker-build/support/install_tmux.sh
-
-# Build: ag, watchman
-sudo -i -u sailor /docker-build/support/build_ag.sh
-sudo -i -u sailor /docker-build/support/build_watchman.sh
-
-# Install: ag, watchman
-/docker-build/support/install_ag.sh
-/docker-build/support/install_watchman.sh
+# Build, install: ag, tmux, watchman
+/docker-build/support/build_ag.sh
+/docker-build/support/build_tmux.sh
+/docker-build/support/build_watchman.sh
 
 # Cleanup
 apt-get -y remove --auto-remove autoconf automake dpkg-dev g++ gcc libc6-dev libpcre3-dev liblzma-dev pkg-config software-properties-common zlib1g-dev
