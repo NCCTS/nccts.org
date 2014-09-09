aaf33b0790fb45bff428c847ae0515ae69a268ff
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2014-09-09 11:51:01 -0500

    for now, hardwire to stable-410.0.0, to avoid accidental variance between local,rackspace setups

---
+++ docker/clojure/setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+export HOME=/root
+
+# Install entry script
+cp /docker-build/support/entry.sh /usr/local/bin/entry.sh
+
+# Install packages
+apt-get update
+apt-get -y install software-properties-common
+add-apt-repository -y ppa:webupd8team/java
+apt-get update
+echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+apt-get install -y oracle-java7-installer
+
+# Setup Leiningen / Clojure environment for unprivileged user 'sailor'
+sudo -i -u sailor /docker-build/support/user_sailor.sh
+
+# Cleanup
+apt-get -y remove --auto-remove software-properties-common
+apt-get -y autoclean
+apt-get -y clean
+apt-get -y autoremove
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+# ENTRYPOINT, CMD inherited from nccts/baseimage
