527c45db8b375361cda40051738d22356b5085ba
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2014-09-09 13:21:46 -0500

    pass the tmux session name via ENTRYPOINT (varies by image)

--- docker/baseimage/support/entry.sh
+++ docker/baseimage/support/entry.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+sess_name=$1
+shift
+
 if [ -n "$*" ]; then
-    tmux_cmd=(tmux new-session -s base "$*")
+    tmux_cmd=(tmux new-session -s "$sess_name" "$*")
     su_cmd=(su -c "$(printf "%q " "${tmux_cmd[@]}")" - sailor)
     bash -l -c "$(printf "%q " "${su_cmd[@]}")"
 else
-    bash -l -c "su -c 'tmux new-session -s base' - sailor"
+    bash -l -c "su -c 'tmux new-session -s $sess_name' - sailor"
 
     # Alternatively, allow `command exit` in a 'sailor'
     # tmux/shell to drop back to a root shell
     # ------------------------------------------------------------
-    # bash -l -c "su -c 'tmux new-session -s base' - sailor; bash"
+    # bash -l -c "su -c 'tmux new-session -s $sess_name' - sailor; bash"
 fi
