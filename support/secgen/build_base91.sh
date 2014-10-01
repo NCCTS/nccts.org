2cccc7f77d916c637a62bc181b60afa003c38d00
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2014-10-01 17:05:16 -0500

    bash style

--- support/secgen/build_base91.sh
+++ support/secgen/build_base91.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+script_path=$(cd $( dirname "${BASH_SOURCE[0]}" ) && pwd)
+
+cd $script_path
 tar xvzf ./base91-0.6.0.tar.gz
 cd base91-0.6.0
 make
