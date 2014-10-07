a5d1550be98484538bd7a277927f0b1338193f16
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2014-10-06 19:41:30 -0500

    reorg fleet/prod units and scripts - will "strap" the scripts into the cluster member/s by git clone/pull invoked via fleetctl unit rather than pushing to cluster member/s with rsync

---
+++ fleet/prod/scripts/repo_strap.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+repo_dir=$1
+repo_url=$2
+
+mkdir -p /home/core/repos
+cd /home/core/repos
+
+if [ -d "./$repo_dir" ]; then
+    echo "pulling repo..."
+    cd ./$repo_dir
+    git checkout master
+    git pull
+    latest_tag=$(git describe --tags 2>/dev/null)
+    if [ -n "$latest_tag" ]; then
+        git checkout $latest_tag
+    else
+        echo "no tags, sticking with HEAD on master..."
+    fi
+else
+    echo "cloning repo..."
+    git clone $repo_url ./$repo_dir
+    chown -R core:core ./$repo_dir
+    cd ./$repo_dir
+    latest_tag=$(git describe --tags 2>/dev/null)
+    if [ -n "$latest_tag" ]; then
+        git checkout $latest_tag
+    else
+        echo "no tags, sticking with HEAD on master..."
+    fi
+fi
+
+chown -R core:core /home/core/repos
