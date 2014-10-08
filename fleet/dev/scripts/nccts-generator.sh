2fe2d54decb4ed6e501aaf1205f1daa6d72c9eb4
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2014-10-08 01:53:50 -0500

    ensure clojure/build subdir exists

--- fleet/dev/scripts/nccts-generator.sh
+++ fleet/dev/scripts/nccts-generator.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 
 docker run -it --rm --volumes-from data -p 7888:7888 --name generator nccts/clojure:latest '\
+    mkdir -p /home/sailor/nccts.org/site/source/clojure/build ; \
+
     watchman watch /home/sailor/nccts.org/site/source/clojure/build/ ; \
 
     watchman -- trigger /home/sailor/nccts.org/site/source/clojure/build/ rsync-generator \
