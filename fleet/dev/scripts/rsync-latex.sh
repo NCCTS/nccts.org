993e177c5800333bee510e313b5417135a1624f7
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2014-10-08 02:10:48 -0500

    rsync-latex should not sync the latexmlpost output to site/build

--- fleet/dev/scripts/rsync-latex.sh
+++ fleet/dev/scripts/rsync-latex.sh
@@ -4,5 +4,7 @@
     --exclude '*.aux' \
     --exclude '*.log' \
     --exclude '*.xml' \
+    --exclude 'companion/' \
+    --exclude 'manual/' \
     /home/sailor/nccts.org/site/source/tex/build \
     /home/sailor/nccts.org/site/
