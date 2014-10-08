71afd1095535f0350e66ce04b6e55113cc77f629
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2014-10-08 01:54:17 -0500

    beginnings of templated fragments,pages

--- site/source/clojure/src/org/nccts/nccts_site.clj
+++ site/source/clojure/src/org/nccts/nccts_site.clj
@@ -1,7 +1,30 @@
-(ns org.nccts.nccts-site)
+(ns org.nccts.nccts-site
+  (:require [hiccup.core            :as hiccup]
+            [net.cgrand.enlive-html :as enlive-html]
+            [stasis.core            :as stasis]
+            [stencil.core           :as stencil]))
 
-(def hello "world")
+(defn t-slurp [part]
+  (slurp (str "site/source/clojure/templates/" part)))
+
+(def fragments {:header
+                (t-slurp "header.html")
+
+                :footer
+                (t-slurp "footer.html")})
+
+(def pages {"/index.html"
+            (t-slurp "index.html")
+
+            "/clcc/index.html"
+            (t-slurp "clcc/index.html")})
+
+(def target-dir "site/source/clojure/build")
+
+(defn export []
+  (stasis/empty-directory! target-dir)
+  (stasis/export-pages pages target-dir))
 
 (defn -main
   [& args]
-  (println "hello, world!"))
+  (export))
