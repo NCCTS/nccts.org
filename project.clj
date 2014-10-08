05351ad79f3ef59c2d28a0186c1cbbc1086427af
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2014-10-07 22:25:44 -0500

    fleshing out the project.clj

--- project.clj
+++ project.clj
@@ -1,11 +1,20 @@
-(defproject nccts-site "0.0.1-SNAPSHOT"
+(defproject org.nccts/nccts-site "0.0.1-SNAPSHOT"
   :description "Site generator for nccts.org"
-  :license {:name "Eclipse Public License 1.0"
-            :url "https://www.eclipse.org/legal/epl-v10.html"}
+  :license {:distribution :repo
+            :comments "same as Clojure"
+            :name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/NCCTS/nccts.org"
 
-  :dependencies [[org.clojure/clojure "1.7.0-alpha2"]]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha2"]
+                 [stasis "2.2.2"]]
+
+  :main org.nccts.nccts-site
+
+  :min-lein-version "2.5.0"
 
   :plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]]
 
-  :source-paths ["site/source/clojure"])
+  :resource-paths ["site/source"]
+
+  :source-paths ["site/source/clojure/src"])
