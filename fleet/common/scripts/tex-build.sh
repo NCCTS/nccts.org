acec9009c309a9c2d0127f411b82f6fb35366c32
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2014-10-09 14:14:54 -0500

    cp some scripts to fleet/common/scripts, will dedupe later

---
+++ fleet/common/scripts/tex-build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+task=$1
+part_name=$2
+
+source_tex="/home/sailor/nccts.org/site/source/tex"
+
+if [ "$task" = "pdf" ]; then
+    pdflatex \
+        -output-directory "$source_tex/build/clcc" \
+        "$source_tex/clcc-$part_name.tex"
+elif [ "$task" = "xml" ]; then
+    latexml \
+        --output="$source_tex/build/clcc/clcc-$part_name.xml" \
+        "$source_tex/clcc-$part_name.tex"
+elif [ "$task" = "html" ]; then
+    latexmlpost \
+        --destination="$source_tex/build/clcc/$part_name/index.html" \
+        --format=html5 "$source_tex/build/clcc/clcc-$part_name.xml" ; \
+else
+    echo "no matching task, exiting..."
+    exit 1
+fi
