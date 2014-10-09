031a976f5aa6d96ffd130c27bbcd8c044cff2d9a
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2014-10-09 13:25:32 -0500

    fleet reorg, in progress

---
+++ fleet/prod/common/scripts/repo_strap.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/bash
+
+repo_dir=$1
+repo_url=$2
+
+mkdir -p /home/core/repos
+cd /home/core/repos
+
+if [ -d "./$repo_dir" ]; then
+    echo "pulling repo..."
+    cd ./$repo_dir
+    git checkout master
+    git pull
+    if [ "$3" = "tagged" ]; then
+        latest_tag=$(git describe --abbrev=0 2>/dev/null)
+        if [ -n "$latest_tag" ]; then
+            git checkout $latest_tag
+        else
+            echo "no tags, sticking with HEAD on master..."
+        fi
+    else
+        echo "tagged not specified, sticking with HEAD on master..."
+    fi
+else
+    echo "cloning repo..."
+    git clone $repo_url ./$repo_dir
+    chown -R core:core ./$repo_dir
+    cd ./$repo_dir
+    if [ "$3" = "tagged" ]; then
+        latest_tag=$(git describe --abbrev=0 2>/dev/null)
+        if [ -n "$latest_tag" ]; then
+            git checkout $latest_tag
+        else
+            echo "no tags, sticking with HEAD on master..."
+        fi
+    else
+        echo "tagged not specified, sticking with HEAD on master..."
+    fi
+fi
+
+chown -R core:core /home/core/repos
