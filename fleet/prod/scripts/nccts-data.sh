fe0bc36edc279a8d32fadf03df95404548be0c50
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2014-10-13 01:54:33 -0500

    hotfix adjustments to old prod scripts

--- fleet/prod/scripts/nccts-data.sh
+++ fleet/prod/scripts/nccts-data.sh
@@ -3,5 +3,5 @@
 _di=$(docker inspect data 2>/dev/null)
 if [ "$?" -eq "1" ]; then
     docker run -it -v /home/sailor/nccts.org --name data nccts/nccts-data:latest '\
-        clone_site.sh'
+        clone_site.sh tagged'
 fi
--- fleet/prod/scripts/nccts-update.sh
+++ fleet/prod/scripts/nccts-update.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 docker run -it --rm --volumes-from data nccts/nccts-data:latest '\
-    pull_site.sh'
+    pull_site.sh tagged'
