a5001a33ceac910986920ac91e84782396b1841a
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2014-12-31 03:34:08 -0600

    invoke "build" sub-fig rather than running docker cli directly

--- fig/prod/common/scripts/build.sh
+++ fig/prod/common/scripts/build.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
-docker run \
-       --name build-data \
-       -v /home/sailor/nccts.org:/home/sailor/nccts.org \
-       quay.io/nccts/nccts-data
+script_path () {
+    local scr_path=""
+    local dir_path=""
+    local sym_path=""
+    # get (in a portable manner) the absolute path of the current script
+    scr_path=$(cd -P -- "$(dirname -- "$0")" && pwd -P) && scr_path=$scr_path/$(basename -- "$0")
+    # if the path is a symlink resolve it recursively
+    while [ -h $scr_path ]; do
+        # 1) cd to directory of the symlink
+        # 2) cd to the directory of where the symlink points
+        # 3) get the pwd
+        # 4) append the basename
+        dir_path=$(dirname -- "$scr_path")
+        sym_path=$(readlink $scr_path)
+        scr_path=$(cd $dir_path && cd $(dirname -- "$sym_path") && pwd)/$(basename -- "$sym_path")
+    done
+    echo $scr_path
+}
 
-docker run \
-       --rm \
-       --volumes-from build-data \
-       --name static \
-       quay.io/nccts/baseimage \
-       '/home/sailor/nccts.org/fig/prod/common/scripts/rsync-static.sh'
+script_dir=$(dirname -- "$(script_path)")
+cd $script_dir/../../../build/prod/
 
-docker run \
-       --rm \
-       --volumes-from build-data \
-       --name latex \
-       quay.io/nccts/latex \
-       '/home/sailor/nccts.org/fig/prod/common/scripts/run-tex.sh'
+fig up builddata
+fig up static
+fig up latex
+fig up generator
 
-docker run \
-       --rm \
-       --volumes-from build-data \
-       --name generator \
-       quay.io/nccts/clojure \
-       '/home/sailor/nccts.org/fig/prod/common/scripts/run-clj.sh'
-
-docker rm build-data
+fig rm --force -v
