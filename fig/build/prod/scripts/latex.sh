4dd6fa1794e8fcac29352d7bc1ca960880417e37
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2015-01-06 04:29:45 -0600

    .

--- fig/build/prod/scripts/latex.sh
+++ fig/build/prod/scripts/latex.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-export source_tex=/home/sailor/nccts.org/site/source/tex
-export build_prod_scripts=/home/sailor/nccts.org/fig/build/prod/scripts
+export source_tex="/home/sailor/nccts.org/site/source/tex"
+export build_com_scripts="/home/sailor/nccts.org/fig/build/common/scripts"
 
 mkdir -p $source_tex/build
 mkdir -p $source_tex/build/clcc
 
-$build_prod_scripts/tex-build.sh pdf manual
-$build_prod_scripts/tex-build.sh xml manual
-$build_prod_scripts/tex-build.sh html manual
+$build_com_scripts/tex-build.sh pdf manual
+$build_com_scripts/tex-build.sh xml manual
+$build_com_scripts/tex-build.sh html manual
 
-$build_prod_scripts/tex-build.sh pdf companion
-$build_prod_scripts/tex-build.sh xml companion
-$build_prod_scripts/tex-build.sh html companion
+$build_com_scripts/tex-build.sh pdf companion
+$build_com_scripts/tex-build.sh xml companion
+$build_com_scripts/tex-build.sh html companion
 
-/home/sailor/nccts.org/fig/build/common/scripts/rsync-latex.sh
+$build_com_scripts/rsync-latex.sh
